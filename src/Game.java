7a2ab7efdc06fdddaf1f12996cf4a41ecc2ed7e3
Tikhon <tixon2002@gmail.com>
2025-01-01 15:14:02 +0000

    Clear out main

--- src/Game.java
+++ src/Game.java
@@ -4,26 +4,7 @@ import java.util.Random;
 public class Game {
 	static Namings namings = new Namings();
 	
-	public static void main(String [] args) 
-    {
-		Language newLang = new Language();
-		
-		System.out.println(newLang.languageName);
-		for (int i = 0; i < 100; i++) {
-			String name = newLang.getGivenName() + " " + newLang.getFamilyName();
-			System.out.println("\t" + name);
-		}
-		
-//		World newWorld = generateRandomWorld();
-//		
-//		for (Region region : newWorld.regions) {
-//			System.out.println("Adjacencies of " + region.regionName + ": ");
-//			for (Region adjacency : region.adjacentRegions) {
-//				System.out.println("\t" + adjacency.regionName);
-//			}
-//		}
-		
-    }
+	public static void main(String [] args) {}
 	
 	static // Generate a Random World
 	World generateRandomWorld() {
